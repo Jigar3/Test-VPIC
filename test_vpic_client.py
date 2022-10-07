3c14fd0de30d80bb6e95540c6d1440af0a939563
Jigar3 <jigar@nirvanatech.com>
2022-10-07 23:38:04 +0530

    new api

--- test_vpic_client.py
+++ test_vpic_client.py
@@ -1,8 +1,8 @@
 import pytest
 import vpic_client
 
-def test_get_vin_record():
-    assert vpic_client.get_vin_record("1FUJGLDR3HLHG7532") == 'FREIGHTLINER'
+# def test_get_vin_record():
+#     assert vpic_client.get_vin_record("1FUJGLDR3HLHG7532") == 'FREIGHTLINER'
 
 def test_random_api():
     assert vpic_client.random_api() == 1
